09919a381df74f869b4c7f171609b6b42e6ca34d
ptdot <92151608+ptdot@users.noreply.github.com>
2022-09-22 21:22:37 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sec-git https://github.com/kenzok8/openwrt-packages
+echo src git https://github.com/kenzok8/openwrt-packages
